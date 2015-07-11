b10dd00769eb2b7e88ca5369a204b6ced01047a9
Gregory Dubois-Felsmann <gpdf@ipac.caltech.edu>
2015-07-11 08:16:49 -0700

    Corrected errors reported by flake8

--- sprint_tutorial/tests/test_compute.py
+++ sprint_tutorial/tests/test_compute.py
@@ -7,9 +7,11 @@ from sprint_tutorial.compute import my_sum
 
 def test_my_sum():
     assert_equal(my_sum(0, 0), 0)
-    
-def test_my_sum1():    
+
+
+def test_my_sum1():
     assert_equal(my_sum(3, 5), 8)
-    
+
+
 def test_my_sum2():
     assert_equal(my_sum(5, 5), 10)
