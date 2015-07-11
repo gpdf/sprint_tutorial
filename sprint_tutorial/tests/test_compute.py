08ec417e42261076c934495e2fc754d943f73817
Gregory (D) <gpdf@vpn-248-14.caltech.edu>
2015-07-11 08:03:45 -0700

    Add additional tests

--- sprint_tutorial/tests/test_compute.py
+++ sprint_tutorial/tests/test_compute.py
@@ -7,3 +7,9 @@ from sprint_tutorial.compute import my_sum
 
 def test_my_sum():
     assert_equal(my_sum(0, 0), 0)
+    
+def test_my_sum1():    
+    assert_equal(my_sum(3, 5), 8)
+    
+def test_my_sum2():
+    assert_equal(my_sum(5, 5), 10)
